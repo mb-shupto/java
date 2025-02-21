ea7bbd33540ae023f324191fc6bc3764097e92f7
MD. MASUM-UL-BARI SUPTO <mr.shupto09@gmail.com>
2025-02-22 03:42:45 +0600

    Another attempt for access methods with an object.

--- Java Class/src/main/java/basic/java/Car.java
+++ Java Class/src/main/java/basic/java/Car.java
@@ -1,4 +1,18 @@
 package basic.java;
 
 public class Car {
+    String brand;
+    int speed;
+
+    //constructor
+    Car(String b, int s){
+        brand = b;
+        speed = s;
+    }
+
+    // Method to display car info
+    void showCarInfo(){
+        System.out.println("Car brand: " + brand);
+        System.out.println("Car speed: "+ speed + "km/h");
+    }
 }
