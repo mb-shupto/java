d91765d6218254d20a675bb3befb4ae75d2ac9dd
MD. MASUM-UL-BARI SUPTO <mr.shupto09@gmail.com>
2025-02-22 03:38:56 +0600

    Added comment for better explaination.

---
+++ Java Class/src/main/java/basic/java/Car.java
@@ -0,0 +1,4 @@
+package basic.java;
+
+public class Car {
+}
--- Java Class/src/main/java/basic/java/multiClassMain.java
+++ Java Class/src/main/java/basic/java/multiClassMain.java
@@ -1,10 +1,13 @@
 package basic.java;
 
 public class multiClassMain {
-    public void theCage(){
+    //method to display my message
+    public void theCage()
+    {
         System.out.println("I love the show 'The Cage'. Their band collection is so much great!");
     }
-    public void choosen(String bandName){
+    public void choosen(String bandName)
+    {
         System.out.println("Specially there's this band called " + bandName);
     }
 
