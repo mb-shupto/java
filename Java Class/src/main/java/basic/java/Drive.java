2f059625c8b32a99f0fff903d7f08b15f9e70994
MD. MASUM-UL-BARI SUPTO <mr.shupto09@gmail.com>
2025-02-22 03:46:34 +0600

    created object of a class and call the method

--- Java Class/src/main/java/basic/java/Drive.java
+++ Java Class/src/main/java/basic/java/Drive.java
@@ -1,4 +1,13 @@
 package basic.java;
 
 public class Drive {
+    public static void main(String[] args) {
+        // creating an object of Car class inside Drive class
+        Car car1 = new Car("Toyota", 100);
+        Car car2 = new Car("BMW", 250);
+
+        // calling the method to display car details
+        car1.showCarInfo();
+        car2.showCarInfo();
+    }
 }
