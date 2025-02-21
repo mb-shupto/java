eccddf9c7dfb712e50c44996815866937e9ab731
MD. MASUM-UL-BARI SUPTO <mr.shupto09@gmail.com>
2025-02-22 03:40:15 +0600

    added comment for better code explaination.

--- Java Class/src/main/java/basic/java/multiClassSecond.java
+++ Java Class/src/main/java/basic/java/multiClassSecond.java
@@ -2,7 +2,10 @@ package basic.java;
 
 public class multiClassSecond {
     public static void main(String[] args) {
+        // creating an object of the multiClassMain Class
         multiClassMain band =new multiClassMain();
+
+        // calling the method to display my message from another class
         band.theCage();
         band.choosen("Dusk and Dawn.");
     }
