fc7cc4d0b10d542db29fee1b87f7dc17e641f84e
MD. MASUM-UL-BARI SUPTO <mr.shupto09@gmail.com>
2025-02-25 01:47:22 +0600

    controller class file

---
+++ Java Class/src/main/java/encapsulation/Main.java
@@ -0,0 +1,9 @@
+package encapsulation;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.setName("John");
+        System.out.println(person.getName());
+    }
+}
