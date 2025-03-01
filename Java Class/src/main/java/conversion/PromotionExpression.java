b8f3b32cfb140fda5a9f919998c0e5837a78b982
MD. MASUM-UL-BARI SUPTO <mr.shupto09@gmail.com>
2025-03-02 00:44:46 +0600

    promotion to expression

--- Java Class/src/main/java/conversion/PromotionExpression.java
+++ Java Class/src/main/java/conversion/PromotionExpression.java
@@ -1,4 +1,12 @@
 package conversion;
 
 public class PromotionExpression {
+
+    public static void main(String[] args) {
+        int num = 5;
+        double num1 = 2.55;
+        double result = num + num1;
+
+        System.out.println(result);
+    }
 }
