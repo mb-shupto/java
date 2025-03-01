eb5ab215b27ff9f131d73f8e6789f9edda2c7a5d
MD. MASUM-UL-BARI SUPTO <mr.shupto09@gmail.com>
2025-03-02 00:44:12 +0600

    widening the range

--- Java Class/src/main/java/conversion/Explicit.java
+++ Java Class/src/main/java/conversion/Explicit.java
@@ -1,4 +1,10 @@
 package conversion;
 
 public class Explicit {
+    public static void main(String[] args) {
+        double pi = 3.14159;
+        int intPi = (int) pi;
+        System.out.println(intPi);
+    }
+
 }
