172b06934ffccf26cc6dcd01e69422265e707f6a
MD. MASUM-UL-BARI SUPTO <mr.shupto09@gmail.com>
2025-03-01 22:35:22 +0600

    implicit type conversion

---
+++ Java Class/src/main/java/conversion/Main.java
@@ -0,0 +1,11 @@
+package conversion;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int num = 10;
+        double result = num;
+
+        System.out.println(result);
+    }
+}
